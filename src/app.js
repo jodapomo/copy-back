ad7d487b36f7d302f9ad5c9ac76cfccaf265d646
José Posada <josedanielpm@gmail.com>
2019-07-18 20:02:01 -0500

    connection console log fixed

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ sockets( io );
 mongoose.connect( process.env.MONGO_URI, config.mongooseOptions, ( err ) => {
     if ( err ) throw err;
 
-    console.log( `MongoDB: \x1b[32m%s\x1b[0m`, 'ONLINE' );
+    console.log( `MongoDB: ONLINE` );
 } );
 
-server.listen( process.env.PORT, () => console.log( `Express server on port ${ process.env.PORT }: \x1b[32m%s\x1b[0m`, 'online' ) );
+server.listen( process.env.PORT, () => console.log( `Express server on port ${ process.env.PORT }: ONLINE` ) );
